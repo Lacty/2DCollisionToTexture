d1cd889a6db59400fce7de75850a886f9b93a160
Lacty <akira206@gmail.com>
2016-06-09 18:02:31 +0900

    マウスカーソルの位置を取得できるようにした

--- src/main.cpp
+++ src/main.cpp
@@ -6,28 +6,38 @@
 
 
 // window size
-static int w_width  = 640;
-static int w_height = 480;
+static int window_w  = 640;
+static int window_h = 480;
+
+// mouse posision
+static double mouse_x = 0;
+static double mouse_y = 0;
+void mousePosCallBack(GLFWwindow* window, double xpos, double ypos) {
+  mouse_x = xpos;
+  mouse_y = ypos;
+}
 
 int main() {
   if (!glfwInit()) return -1;
 
-  auto* window = glfwCreateWindow(w_width, w_height, "2DCollisionToTexture", NULL, NULL);
+  auto* window = glfwCreateWindow(window_w, window_h, "2DCollisionToTexture", NULL, NULL);
   if (!window) {
     glfwTerminate();
     return -1;
   }
 
   glfwMakeContextCurrent(window);
+  glfwSetCursorPosCallback(window, mousePosCallBack);
+  
   glClearColor(0.4f, 0.4f, 0.4f, 1.0f);
   
-  glOrtho(0, w_width, w_height, 0, -0.0f, 1.0f);
-  glViewport(0, 0, w_width, w_height);
+  glOrtho(0, window_w, window_h, 0, -0.0f, 1.0f);
+  glViewport(0, 0, window_w, window_h);
   
   while (!glfwWindowShouldClose(window)) {
     glClear(GL_COLOR_BUFFER_BIT);
     
-    drawPoint(0, 0, 10);
+    drawPoint(mouse_x, mouse_y, 10);
     
     glfwSwapBuffers(window);
     glfwPollEvents();
