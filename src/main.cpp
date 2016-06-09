aa672c7762c66a658610dfb312d6d18a57606a68
Lacty <akira206@gmail.com>
2016-06-09 17:31:29 +0900

    create window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,31 @@
 
 #include <iostream>
+#include <GLFW/glfw3.h>
 
+
+// window size
+static int w_width  = 640;
+static int w_height = 480;
 
 int main() {
-  std::cout << "hoge" << std::endl;
+  if (!glfwInit()) return -1;
+
+  auto* window = glfwCreateWindow(w_width, w_height, "2DCollisionToTexture", NULL, NULL);
+  if (!window) {
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+  glClearColor(0.4f, 0.4f, 0.4f, 1.0f);
+  
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  
+  glfwTerminate();
+  return 0;
 }
--- xcode/src/2DCollisionToTexture.build/Debug/2DCollisionToTexture.build/Objects-normal/x86_64/main.d
+++ xcode/src/2DCollisionToTexture.build/Debug/2DCollisionToTexture.build/Objects-normal/x86_64/main.d
@@ -1,1 +1,2 @@
-dependencies: /Users/yanai/Desktop/2DCollisionToTexture/src/main.cpp
+dependencies: /Users/yanai/Desktop/2DCollisionToTexture/src/main.cpp \
+  /Users/yanai/Desktop/2DCollisionToTexture/include/GLFW/glfw3.h
